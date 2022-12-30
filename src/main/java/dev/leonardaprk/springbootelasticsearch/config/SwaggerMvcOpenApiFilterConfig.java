441d4f9ca5b0e94bf1f8a7141339e0c28198168f
Leo Wu <leo.wu.kw@gmail.com>
2022-12-31 01:04:15 +0800

    init swagger

---
+++ src/main/java/dev/leonardaprk/springbootelasticsearch/config/SwaggerMvcOpenApiFilterConfig.java
@@ -0,0 +1,29 @@
+package dev.leonardaprk.springbootelasticsearch.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+
+@Component
+public class SwaggerMvcOpenApiFilterConfig implements WebMvcOpenApiTransformationFilter {
+  @Override
+  public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+    OpenAPI openApi = context.getSpecification();
+    Server localServer = new Server();
+    localServer.setUrl(openApi.getServers().get(0).getUrl() + "/v1/");
+
+    openApi.setServers(Collections.singletonList(localServer));
+    return openApi;
+  }
+
+  @Override
+  public boolean supports(DocumentationType delimiter) {
+    return delimiter.equals(DocumentationType.OAS_30);
+  }
+}
