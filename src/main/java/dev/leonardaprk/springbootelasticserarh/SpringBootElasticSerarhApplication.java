7c4d72a6e741838b288075c87cdf41b2be95eb76
Leo Wu <leo.wu.kw@gmail.com>
2022-12-31 00:04:03 +0800

    init project

---
+++ src/main/java/dev/leonardaprk/springbootelasticserarh/SpringBootElasticSerarhApplication.java
@@ -0,0 +1,13 @@
+package dev.leonardaprk.springbootelasticserarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticSerarhApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootElasticSerarhApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/leonardaprk/springbootelasticserarh/SpringBootElasticSerarhApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonardaprk.springbootelasticserarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticSerarhApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
